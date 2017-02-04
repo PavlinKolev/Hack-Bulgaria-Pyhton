7a90fc135349a137abe1aaff843bc989dfdda016
PavlinKolev <pavkata993@gmail.com>
2017-02-04 15:53:20 +0200

    histogram

--- Week14/Crawler/crawling.py
+++ Week14/Crawler/crawling.py
@@ -1,10 +1,13 @@
+import os
 import json
 from bs4 import BeautifulSoup
 import requests
+import matplotlib.pyplot as plt
+from settings import SERVERS_FREQS_JSON, OUR_HEADERS, START_BG_URL
 
 
-def get_all_links(url="http://register.start.bg/"):
-    resp = requests.get(url)
+def get_all_links(url=START_BG_URL):
+    resp = requests.get(url, headers=OUR_HEADERS)
     soup = BeautifulSoup(resp.content, "html.parser")
 
     links = []
@@ -17,24 +20,51 @@ def get_all_links(url="http://register.start.bg/"):
 
 
 def make_servers_freq_json():
-    session = requests.session()
-    session.keep_alive = False
     links = get_all_links()
     servers_freqs = {}
     for link in links:
         try:
-            resp = session.get(link)
+            resp = requests.head(link, headers=OUR_HEADERS)
             server_name = resp.headers['Server']
-            print(server_name)
+            if server_name.startswith("Apache"):
+                server_name = "Apache"
             if server_name not in servers_freqs:
                 servers_freqs[server_name] = 1
             else:
                 servers_freqs[server_name] += 1
+            print(server_name)
         except Exception:
             pass
-    with open("servers_freqs.json", 'w') as f:
+    with open(SERVERS_FREQS_JSON, 'w') as f:
         f.write(json.dumps(servers_freqs, indent=4))
 
 
+def create_hist(servers_freqs):
+    keys = list(servers_freqs.keys())
+    values = list(servers_freqs.values())
+
+    X = list(range(len(keys)))
+
+    plt.bar(X, list(servers_freqs.values()), align="center")
+    plt.xticks(X, keys)
+
+    plt.title(".bg servers")
+    plt.xlabel("Server")
+    plt.ylabel("Count")
+
+    plt.savefig("histogram.png")
+
+
+def main():
+    if not os.path.exists(SERVERS_FREQS_JSON):
+        make_servers_freq_json()
+
+    servers_freqs = {}
+    with open(SERVERS_FREQS_JSON, 'r') as f:
+        servers_freqs = json.load(f)
+
+    create_hist(servers_freqs)
+
+
 if __name__ == '__main__':
-    make_servers_freq_json()
+    main()
---
+++ Week14/Crawler/settings.py
@@ -0,0 +1,5 @@
+SERVERS_FREQS_JSON = "servers_freqs.json"
+
+OUR_HEADERS = {"User-Agent": "Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.36" }
+
+START_BG_URL = "http://register.start.bg/"
